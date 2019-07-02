5865e568504f07808d67bf1365eed5768be331e6
unknown <david.vitora@gmail.com>
2019-07-02 16:41:27 -0300

    Avatar corrigido

--- src/routes/panel/Panel.jsx
+++ src/routes/panel/Panel.jsx
@@ -1,31 +1,27 @@
 import React, { useState } from 'react';
 import { useObserver } from 'mobx-react-lite'
-import { Layout, Menu, Icon, Dropdown, Button } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
 import { Switch, Route, Link } from "react-router-dom";
 import FormExample from './FormExample.js'
 import DataFlowExample from './DataFlowExample.js'
 import Dashboard from './Dashboard.js'
+import Avatar from './components/Avatar'
 
 const { SubMenu } = Menu;
 const { Header, Sider } = Layout;
-
-const menu = (
-  <Menu>
-    <Menu.Item key="exit">
-      <Link to='/'>
-        <Icon type="logout"/>
-        <span>Logout</span>
-      </Link>
-    </Menu.Item>
-  </Menu>
-);
 
 export default () => {
   const [ collapsed, setCollapsed ] = useState(false)
 
   return useObserver(() => (
     <Layout style={{height:"100vh"}} >
-        <Sider width={200} style={{ background: '#fff' }} collapsed={collapsed}>
+        <Sider 
+        width={200} 
+        style={{ background: '#fff' }} 
+        collapsed={collapsed}
+        theme="dark"
+        breakpoint="md"
+        >
           <Menu
             mode="inline"
             style={{ height: '100%', borderRight: 0 }}
@@ -71,11 +67,9 @@ export default () => {
                 type={collapsed ? 'menu-unfold' : 'menu-fold'}
                 onClick={() => { setCollapsed(!collapsed) }}
               />
-              <Dropdown overlay={menu}>
-                <Button>
-                  Button <Icon type="down" />
-                </Button>
-              </Dropdown>
+              <div style={{ float: 'right' }}>
+                <Avatar/>
+              </div>
             </Menu>
           </Header>
           <Layout style={{ padding: '0 24px 24px' }}>
---
+++ src/routes/panel/components/Avatar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Menu, Icon, Dropdown, Button } from 'antd';
+import { Link } from 'react-router-dom'
+
+const menu = (
+  <Menu>
+    <Menu.Item key="exit">
+      <Link to='/'>
+        <Icon type="logout"/>
+        <span>Logout</span>
+      </Link>
+    </Menu.Item>
+  </Menu>
+)
+
+
+export default () =>
+<Dropdown style={{height: '100%'}} overlay={menu}>
+  <span>Usuário <Icon type="down" /></span>
+</Dropdown>
